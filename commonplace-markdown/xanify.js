6db07ee13481ce02c27003bc5e36f53b59d68c98
complexitycollapse <complexitycollapse@protonmail.com>
2024-08-15 23:52:48 +0100

    xanify lists

--- commonplace-markdown/xanify.js
+++ commonplace-markdown/xanify.js
@@ -1,70 +1,86 @@
-import { Link, Span, Edl } from "@commonplace/core";
-import { paragraphType, documentType, strongType, emphasisType } from "@commonplace/document-model";
+import { Link, Span, Edl, Image, LinkPointer, InlinePointer } from "@commonplace/core";
+import { paragraphType, documentType, strongType, emphasisType, listItemType, listType,
+  markupType
+ } from "@commonplace/document-model";
 
 export function xanify(originalContent, parsedData) {
   
-  const links = [];
+  let linkNameUnique = 0;
+  const links = new Map();
 
-  function getSpans(product) {
-    return iterateSpans(product).map(s => Span(originalContent, s[0], s[1] - s[0] + 1));
+  function addLink(nameish, link) {
+    const name = nameish + ++linkNameUnique;
+    links.set(name, link);
+    return [name, link];
   }
 
+  const convertSpan = (s) => Span(originalContent, s[0], s[1] - s[0] + 1);
+
   function processData(product) {
+    if (Array.isArray(product)) {
+      return product.map(processData).flat();
+    }
 
     switch (product.type) {
       case "paragraph":
-        linkForContent(paragraphType, product.content);
+        return linkAndContent("p", paragraphType, product.content);
         break;
       case "bold":
-        linkForContent(strongType, product.content);
+        return linkAndContent("b", strongType, product.content);
         break;
       case "italic":
-        linkForContent(emphasisType, product.content);
+        return linkAndContent("i", emphasisType, product.content);
         break;
       case "text":
-        // Nothing to do
+        return convertSpan(product.content);
         break;
+      case "link":
+      case "image":
+        return Image(product.src);
+      case "heading":
+      case "ul":
+      case "ol":
+        return processList(product);
       default:
         throw new Error("Cannot xanify type: " + product.type);
     }
   }
 
-  function linkForContent(linkType, content) {
-    const spans = getSpans(content);
-    const plink = Link(linkType, [undefined, spans]);
-    links.push(plink);
-    content.map(processData);
+  function linkAndContent(linkNameish, linkType, content) {
+    const clips = processData(content);
+    const plink = Link(linkType, [undefined, clips]);
+    addLink(linkNameish, plink);
+    return clips;
   }
 
-  const clips = parsedData.map(getSpans).flat();
+  function processList(list) {
+    let clips = [], itemLinkNames = [];
+    for (const item of list.items) {
+      const [itemLink, itemClips] = processListItem(item);
+      const [itemLinkName] = addLink("item", itemLink);
+      itemLinkNames.push(itemLinkName);
+      clips = clips.concat(itemClips);
+    }
 
-  parsedData.forEach(processData);
+    const [listLinkName] = addLink(list, Link(listType, ["items", itemLinkNames.map(n => LinkPointer(n))]));
+
+    // And add list marker style markup to distinguish ul from ol
+    addLink("list marker", Link(markupType, 
+      ["targets", [LinkPointer(listLinkName)]],
+      ["attribute", [InlinePointer("list marker")]],
+      ["value", [InlinePointer(list.type == "ol" ? "numbers" : "bullets")]],
+      ["inheritance", [InlinePointer("direct")]]));
+
+    return clips;
+  }
+
+  function processListItem(item) {
+    const clips = processData(item.content);
+    const link = Link(listItemType, [undefined, clips]);
+    return [link, clips];
+  }
+
+  const clips = processData(parsedData);
 
   return Edl(documentType, clips, links);
-}
-
-function iterateSpans(product) {
-  if (Array.isArray(product)){
-    return product.map(iterateSpans).flat();
-  }
-
-  switch (product.type) {
-    case "paragraph":
-    case "bold":
-    case "italic":
-    case "heading":
-    case "li":
-      return product.content.map(iterateSpans).flat();
-    case "link":
-      return product.text.map(iterateSpans).flat();
-    case "image":
-      return product.alt.map(iterateSpans).flat();
-    case "ul":
-    case "ol":
-      return product.items.map(iterateSpans).flat();
-    case "text":
-      return [product.content];
-    default:
-      throw new Error("Cannot xanify type: " + product.type);
-  }
 }
--- commonplace-markdown/xanify.test.js
+++ commonplace-markdown/xanify.test.js
@@ -1,7 +1,12 @@
 import { describe, test, expect } from "just";
 import { xanify } from "./xanify.js"
 import { parse } from "./parser.js";
-import { emphasisType, paragraphType, strongType } from "@commonplace/document-model";
+import { emphasisType, listItemType, listType, markupType, paragraphType, strongType } from "@commonplace/document-model";
+import { InlinePointer, LinkPointer } from "@commonplace/core";
+
+function getEnd(link, endName) {
+  return link.ends.find(e => e.name === endName);
+}
 
 describe("xanify", () => {
   test("single paragraph", () => {
@@ -9,8 +14,10 @@ describe("xanify", () => {
     
     expect(res.clips).toHaveLength(1);
     expect(res.clips[0]).toMatchObject({origin: "ori", start: 0, length: 9});
-    expect(res.links).toHaveLength(1);
-    expect(res.links[0]).toMatchObject({type: paragraphType, ends: [{name: undefined, pointers: [{origin: "ori", start: 0, length: 9}]}]});
+    expect([...res.links.values()]).toHaveLength(1);
+    expect([...res.links.values()][0]).toMatchObject({
+      type: paragraphType,
+      ends: [{name: undefined, pointers: [{origin: "ori", start: 0, length: 9}]}]});
   });
 
   test("bold", () => {
@@ -18,8 +25,8 @@ describe("xanify", () => {
     
     expect(res.clips).toHaveLength(1);
     expect(res.clips[0]).toMatchObject({origin: "ori", start: 2, length: 9});
-    expect(res.links).toHaveLength(2);
-    expect(res.links.find(l => l.type === strongType)).toMatchObject({
+    expect([...res.links.values()]).toHaveLength(2);
+    expect([...res.links.values()].find(l => l.type === strongType)).toMatchObject({
       type: strongType,
       ends: [{name: undefined, pointers: [{origin: "ori", start: 2, length: 9}]}]});
   });
@@ -29,9 +36,53 @@ describe("xanify", () => {
     
     expect(res.clips).toHaveLength(1);
     expect(res.clips[0]).toMatchObject({origin: "ori", start: 1, length: 9});
-    expect(res.links).toHaveLength(2);
-    expect(res.links.find(l => l.type === emphasisType)).toMatchObject({
+    expect([...res.links.values()]).toHaveLength(2);
+    expect([...res.links.values()].find(l => l.type === emphasisType)).toMatchObject({
       type: emphasisType,
       ends: [{name: undefined, pointers: [{origin: "ori", start: 1, length: 9}]}]});
+  });
+
+  test("ul", () => {
+    const item1Span = {origin: "ori", start: 2, length: 6};
+    const item2Span = {origin: "ori", start: 11, length: 6};
+
+    let res = xanify("ori", parse("- Item 1\n- Item 2"));
+
+    expect(res.clips).toHaveLength(2);
+    expect(res.clips[0]).toMatchObject(item1Span);
+    expect(res.clips[1]).toMatchObject(item2Span);
+
+    const itemLinks = [...res.links.values()].filter(l => l.type === listItemType);
+    expect(itemLinks).toHaveLength(2);
+    expect(itemLinks[0].ends).toMatchObject([{name: undefined, pointers: [item1Span]}]);
+    expect(itemLinks[1].ends).toMatchObject([{name: undefined, pointers: [item2Span]}]);
+
+    const listLink = [...res.links.values()].filter(l => l.type === listType)[0];
+    expect(listLink.ends).toMatchObject([{name: "items", pointers: [LinkPointer("item1"), LinkPointer("item2")]}]);
+
+    const markupLink = [...res.links.values()].filter(l => l.type === markupType)[0];
+    expect(getEnd(markupLink, "value").pointers).toMatchObject([InlinePointer("bullets")]);
+  });
+
+  test("ol", () => {
+    const item1Span = {origin: "ori", start: 3, length: 6};
+    const item2Span = {origin: "ori", start: 13, length: 6};
+
+    let res = xanify("ori", parse("1. Item 1\n2. Item 2"));
+
+    expect(res.clips).toHaveLength(2);
+    expect(res.clips[0]).toMatchObject(item1Span);
+    expect(res.clips[1]).toMatchObject(item2Span);
+
+    const itemLinks = [...res.links.values()].filter(l => l.type === listItemType);
+    expect(itemLinks).toHaveLength(2);
+    expect(itemLinks[0].ends).toMatchObject([{name: undefined, pointers: [item1Span]}]);
+    expect(itemLinks[1].ends).toMatchObject([{name: undefined, pointers: [item2Span]}]);
+
+    const listLink = [...res.links.values()].filter(l => l.type === listType)[0];
+    expect(listLink.ends).toMatchObject([{name: "items", pointers: [LinkPointer("item1"), LinkPointer("item2")]}]);
+
+    const markupLink = [...res.links.values()].filter(l => l.type === markupType)[0];
+    expect(getEnd(markupLink, "value").pointers).toMatchObject([InlinePointer("numbers")]);
   });
 });
